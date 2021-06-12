0fe6d6bf1aa3252cccefc267e029b32e28b77388
Santhosh Varathan <sanvsam11@gmail.com>
2021-06-12 12:09:11 +0530

    stream init

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import React, { useState, useRef } from 'react';
-import Messages from './modules/Messages'
-import socketClient from "socket.io-client"
 
 function App() {
-  const socket = socketClient("http://127.0.0.1:8080")
-  const [messages, setMessages] = useState([])
-  const inputRef = useRef(null)
-  const handleSend = () => {
-    const msg = inputRef.current.value
-    socket.emit('send message', msg)
-  }
-  socket.on('chat message', (msg)=>{
-    setMessages(oldmessages=>[...oldmessages, msg])
-  })
-  return(
-    <div>
-        <Messages Messages={messages} /> 
-        <form id="form" onSubmit={()=>handleSend()}>
-            <input id="input" autoComplete="off" ref={inputRef} /><button>Send</button>
-        </form>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/modules/Message.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Message({message}){
-    return(
-        <li>{message.text}</li>
-    )
-}
